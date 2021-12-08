b1d72a8bd98b3c1c7529504ae35c198d13c576ef
adamhab1 <adam.habash@mail.utoronto.ca>
2021-12-08 18:56:15 -0500

    Minor refactorinng done in gateway package.

--- src/main/java/com/example/phase2/gateways/ReaderGateway.java
+++ src/main/java/com/example/phase2/gateways/ReaderGateway.java
@@ -5,7 +5,7 @@ import com.example.phase2.entity.Recipe;
 import java.io.IOException;
 import java.util.List;
 
-public interface RW {
+public interface ReaderGateway {
 
     List<Recipe> getRecipes() throws IOException, ClassNotFoundException;
 
