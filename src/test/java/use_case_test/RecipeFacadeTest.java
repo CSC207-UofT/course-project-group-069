e527a85e03fd4dac07e2c5c6e884ef5c1312d9b3
danielwhc12 <whc.wang@mail.utoronto.ca>
2021-12-07 20:40:44 -0500

    Refactored tests for User, Recipe and recipe use case.

--- src/test/java/use_case_test/RecipeFacadeTest.java
+++ src/test/java/use_case_test/RecipeFacadeTest.java
@@ -1,5 +1,6 @@
 package use_case_test;
 
+import com.example.phase2.entity.Ingredient;
 import com.example.phase2.entity.User;
 import org.junit.*;
 import com.example.phase2.entity.Recipe;
@@ -16,8 +17,21 @@ import java.util.List;
  */
 
 public class RecipeFacadeTest {
+
+    private Presenter p;
+    private RecipeFacade recipefacade;
+    private User user;
+
+
     @Before
     public void setUp() {
+        p = new Presenter();
+        recipefacade = new RecipeFacade(p);
+        user = new User("daniel", "123");
+        user.fridge.add(new Ingredient("meat",""));
+        user.fridge.add(new Ingredient("pepper",""));
+        user.fridge.add(new Ingredient("butter",""));
+        recipefacade.update(user);
     }
 
     @After
@@ -25,64 +39,11 @@ public class RecipeFacadeTest {
     }
 
     /**
-     * testsetRecipe is the test case for setRecipe
-     */
-    @Test
-    public void testsetRecipe(){
-        Presenter p = new Presenter();
-        RecipeFacade recipefacade = new RecipeFacade(p);
-        List<Recipe> recipes = new ArrayList<>();
-
-        List<String> ingredients1 = new ArrayList<String>();
-        ingredients1.add("apple");
-        ingredients1.add("beef");
-        ingredients1.add("chicken");
-        ingredients1.add("leek");
-        Recipe recipe1 = new Recipe("recipe1",ingredients1, "");
-
-        List<String> ingredients2 = new ArrayList<String>();
-        ingredients2.add("chicken");
-        ingredients2.add("leek");
-        Recipe recipe2 = new Recipe("recipe2",ingredients2, "");
-
-        List<String> ingredients3 = new ArrayList<String>();
-        ingredients3.add("chicken");
-        Recipe recipe3 = new Recipe("recipe3",ingredients3, "");
-
-        recipes.add(recipe1);
-        recipes.add(recipe2);
-        recipes.add(recipe3);
-        assertEquals(recipefacade.recipes,recipes);
-    }
-    /**
      * testgetRecipes is the test case for getRecipes
      */
     @Test
     public void testgetRecipes(){
-        Presenter p = new Presenter();
-        RecipeFacade recipefacade = new RecipeFacade(p);
-        List<Recipe> recipes = new ArrayList<>();
-
-        List<String> ingredients1 = new ArrayList<String>();
-        ingredients1.add("apple");
-        ingredients1.add("beef");
-        ingredients1.add("chicken");
-        ingredients1.add("leek");
-        Recipe recipe1 = new Recipe("recipe1",ingredients1, "");
-
-        List<String> ingredients2 = new ArrayList<String>();
-        ingredients2.add("chicken");
-        ingredients2.add("leek");
-        Recipe recipe2 = new Recipe("recipe2",ingredients2, "");
-
-        List<String> ingredients3 = new ArrayList<String>();
-        ingredients3.add("chicken");
-        Recipe recipe3 = new Recipe("recipe3",ingredients3, "");
-
-        recipes.add(recipe1);
-        recipes.add(recipe2);
-        recipes.add(recipe3);
-        assertEquals(recipefacade.getRecipes("recipe3"),recipe3);
+        assertEquals(recipefacade.getRecipes("Steak").getRecipeName(),"Steak");
     }
 
     /**
@@ -90,10 +51,17 @@ public class RecipeFacadeTest {
      */
     @Test
     public void testUpdate(){
-        Presenter p = new Presenter();
-        RecipeFacade recipefacade = new RecipeFacade(p);
-        User user = new User("daniel", "123");
-        recipefacade.update(user);
         assertEquals(recipefacade.currentUser,user);
+    }
+
+
+    /**
+     * testUpdate is the test case for findRecipe
+     */
+    @Test
+    public void testfindRecipe(){
+        recipefacade.findRecipe();
+        assertEquals(p.getAvailableRecipes(),"Steak | ");
+
     }
 }
